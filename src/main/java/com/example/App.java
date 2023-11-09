2b5734d61ce4283320a52963cf5ca835e11e91a6
Vladyslav Prokhnitskyi <v.prokhnitskyi@Vladyslavs-MacBook-Pro.local>
2023-11-09 20:02:23 +0200

    init commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    @Test
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

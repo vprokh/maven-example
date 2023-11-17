1048426974e4596b44777038ffd6183185ccb843
vprokh <proh8530@gmail.com>
2023-11-17 12:33:36 +0200

    remove redundant import and apply formatting for Main class

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,18 +1,10 @@
 package com.example;
 
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    @Test
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
